8d0e7a1d0754bf0faa4b5ba6911420e1f0409f33
Knobvz <reillon.2002@gmail.com>
2024-10-15 15:52:13 -0300

    add images

--- back/internal/models/activities.go
+++ back/internal/models/activities.go
@@ -5,9 +5,12 @@ import (
 )
 
 type Activity struct {
-	ActivityID  int       `json:"id" gorm:"primaryKey;autoIncrement;not null"`
-	Name        string    `json:"name"`
-	Atype       string    `json:"type"`
+	ActivityID int    `json:"id" gorm:"primaryKey;autoIncrement;not null"`
+	Image      []byte `json:"image"`
+	Name       string `json:"name"`
+	Atype      string `json:"type"`
+	//Subcategoria
+	//Personas Máximas
 	Description string    `json:"description"`
 	StartDate   time.Time `json:"startDate"`
 	EndDate     time.Time `json:"endDate"`
