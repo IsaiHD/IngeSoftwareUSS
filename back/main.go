83bba73a3215d96c449575683899e50ec01ba786
Knobvz <reillon.2002@gmail.com>
2024-10-18 16:36:41 -0300

    Update main.go

--- back/main.go
+++ back/main.go
@@ -23,7 +23,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:5500"},
+		AllowOrigins:     []string{"https://recomendaciones-uss.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
